ed8035fed822014a6ae380788327b618cd530216
Romario Joseph <josephromario@rocketmail.com>
2021-09-24 14:30:04 +0700

    layouting and more refinement

--- src/routes/transporter/index.js
+++ src/routes/transporter/index.js
@@ -1,16 +1,24 @@
 import { Switch, Route } from 'react-router-dom';
+import { Space } from 'antd';
+
 import { Header } from 'components/transporter/header';
 import { TrucksTable } from 'pages/transporter/trucks';
 import { DriversTable } from 'pages/transporter/drivers';
+import { TruckDetail } from 'pages/transporter/trucks/detail';
+import { DriverDetail } from 'pages/transporter/drivers/detail';
+import { Shipment } from 'pages/dashboard/shipment';
 
 export function TransporterRoute() {
   return (
-    <>
+    <Space className="pd-space" direction="vertical" size="large">
       <Header />
       <Switch>
         <Route exact path="/transporter/trucks" component={TrucksTable} />
         <Route exact path="/transporter/drivers" component={DriversTable} />
+        <Route exact path="/transporter/shipment" component={Shipment} />
+        <Route exact path="/transporter/trucks/:id" component={TruckDetail} />
+        <Route exact path="/transporter/drivers/:id" component={DriverDetail} />
       </Switch>
-    </>
+    </Space>
   );
 }
